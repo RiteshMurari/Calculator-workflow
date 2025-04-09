63308a0e57c917e97e438a7462c8b4bea169d2c6
RiteshMurari <143247526+RiteshMurari@users.noreply.github.com>
2025-04-09 15:14:45 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+a = 10
+b = 5
+
+print("🔢 Python Calculator")
+print(f"Addition: {a + b}")
+print(f"Subtraction: {a - b}")
+print(f"Multiplication: {a * b}")
+print(f"Division: {a / b}")
+print(f"Modulus: {a % b}")
